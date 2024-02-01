4cdbaf6f04246d81b7b6adb1b29c13c7e5e331d2
aj595s <132955187+aj595s@users.noreply.github.com>
2024-02-01 19:18:25 +0000

    Finished all Tasks

--- include/csc232.h
+++ include/csc232.h
@@ -18,9 +18,9 @@
 #define TRUE 1
 #define EXECUTE_BLOCK FALSE
 
-#define FINISHED_PART_1 FALSE
-#define FINISHED_PART_2 FALSE
-#define FINISHED_PART_3 FALSE
+#define FINISHED_PART_1 TRUE
+#define FINISHED_PART_2 TRUE
+#define FINISHED_PART_3 TRUE
 
 #include <algorithm>
 #include <cassert>
@@ -60,12 +60,36 @@ using std::setw;
  */
 namespace csc232
 {
-    // Add any user-defined functions prescribed in your assignment below
-    // TODO: 1.1 Declare the Shape interface below
+    class Shape{
+        public:
+        virtual double area() const = 0; 
+        virtual double perimeter() const =0; 
 
-    // TODO: 2.1 Provide an inline definition of the Square class below
+    };
 
-    // TODO: 3.1 Provide an inline definition of the Circle class below
+    class Square: public Shape {
+        public: 
+            double area() const override{
+               return (side*side); 
+            }
+            double perimeter() const override{
+                return (4*side); 
+            }
+        private: 
+            double side{1.0};
+    };
+
+    class Circle: public Shape{
+        public: 
+            double area() const override{
+                return (M_PI*(radius*radius));
+            }
+            double perimeter() const override{
+                return (2*M_PI*(radius*radius));
+            } 
+        private: 
+            double radius{1.0};
+    };
 
     // DO NOT Modify anything below this line
 } // namespace csc232
